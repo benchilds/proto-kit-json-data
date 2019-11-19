eb42786876426a01e561038f124f7dfcb32af0ef
Ben <ben@benchilds.uk>
2019-11-19 11:43:08 +0000

    Slight diversion whilst understanding rendering queue

--- app/routes.js
+++ app/routes.js
@@ -6,6 +6,28 @@ const fs = require("fs-extra")
 // Functions
 // #############################################################################
 
+// Set defaults if anything missing (usually due to deep-linking)
+function setDefaults(req) {
+  
+  console.log(`req.session.data.version: ${req.session.data.version}`)
+  console.log(`req.session.data.sort: ${req.session.data.sort}`)
+
+  // Version
+  if (!req.session.data.version) {
+    console.log('Set default for version')
+    req.session.data.version = 1
+    console.log(`req.session.data.version: ${req.session.data.version}`)
+  }
+  
+  // Sorting
+  if (req.session.data.version > 2 && !req.session.data.sort) {
+    console.log('Set default for sort')
+    req.session.data.sort = 'age'
+    console.log(`req.session.data.sort: ${req.session.data.sort}`)
+  }
+  
+}
+
 // Load JSON data from file ----------------------------------------------------
 
 // fileName excludes path but includes extension e.g. file.json
@@ -21,17 +43,29 @@ function loadJSONFromFileSync(fileName) {
 // Routes
 // #############################################################################
 
+// GET /people|person ----------------------------------------------------------
+// Basically just to catch and handle deep-linking
+
+router.get(/(people|person)/, function (req, res) {
+  
+  console.log(`Route: GET /${req.params[0]} --------------------------------------------`)
+  setDefaults(req)
+  
+  res.render(req.params[0])
+  
+})
+
 // / ---------------------------------------------------------------------------
 
 router.get("/", function (req, res) {
 
-  console.log('---------------------------------------------------------------')
-  // console.log(moment().format("DD MMM, HH:mm:ss") + '] Route: /')
+  console.log('Route: / ------------------------------------------------------')
 
   // Unset session data
   req.session.data = {}
 
   // Load and store people from JSON
+  // TODO Switch to promise version
   let peopleData = loadJSONFromFileSync('people.json');
   req.session.data.people = peopleData
 
@@ -41,23 +75,28 @@ router.get("/", function (req, res) {
 
 // /people ---------------------------------------------------------------------
 
-router.get("/people", function (req, res) {
+router.post("/people", function (req, res) {
+  
+  console.log('Route: /people ------------------------------------------------')
+  setDefaults(req)
 
-  console.log('---------------------------------------------------------------')
-  // console.log(moment().format("DD MMM, HH:mm:ss") + '] Route: /people')
+  await function pause(ms) {
+    console.log("Pause starting...")
+    setTimeout(function() { console.log("Pause done"); }, ms);
+  }
+
+  pause(3000);
 
   res.render('people')
-
+  
 })
 
 // /person ---------------------------------------------------------------------
 
 router.post("/person", function (req, res) {
-
-  console.log('---------------------------------------------------------------')
-  // console.log(moment().format("DD MMM, HH:mm:ss") + '] Route: /people')
-
-  // let personId = req.session.data.
+  
+  console.log('Route: /person ------------------------------------------------')
+  setDefaults(req)
 
   res.render('person')
 
