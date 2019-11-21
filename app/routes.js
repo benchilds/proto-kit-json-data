a461b2dbc2a757d13678ca305128d047a1ce77e3
Ben <ben@benchilds.uk>
2019-11-21 10:19:19 +0000

    Sorting and combining data done

--- app/routes.js
+++ app/routes.js
@@ -56,65 +56,99 @@ router.post("/people", function (req, res) {
   
   console.log('Route: /people ------------------------------------------------')
 
-  // function pause(ms) {
-  //   console.log("Pause starting...")
-  //   setTimeout(function() { console.log("Pause done"); }, ms);
-  // }
-  // pause(3000);
-
-  // v.3: Sort the data 
+  // v.3: Sort the data
+  
   if (req.session.data.version > 2) {
     
+    // Sorting arrays is done with the native array.sort() function, which
+    // iteratively compares an array item with the next array item and then
+    // changes the order depending on the outcome
+
     if (req.session.data.sort == 'age') {
+
       req.session.data.people.sort((a, b) => a.age - b.age);
+
     } else if (req.session.data.sort == 'projects') {
+
       req.session.data.people.sort((a, b) => a.num_projects - b.num_projects);
+
     } else if (req.session.data.sort == 'first-name') {
-      req.session.data.people.sort((a, b) => a.name.first < b.name.first);
+
+      req.session.data.people.sort(function (personA, personB) {
+        let a = personA.name.first.toLowerCase()
+        let b = personB.name.first.toLowerCase()
+        if (a > b) {
+          return 1
+        } else if (a < b) {
+          return -1
+        } else {
+          return 0
+        }
+      });
+
     } else if (req.session.data.sort == 'last-name') {
-      req.session.data.people.sort((a, b) => a.name.last < b.name.last);
+
+      req.session.data.people.sort(function (personA, personB) {
+        let a = personA.name.last.toLowerCase()
+        let b = personB.name.last.toLowerCase()
+        if (a > b) {
+          return 1
+        } else if (a < b) {
+          return -1
+        } else {
+          return 0
+        }
+      });
+
     } else if (req.session.data.sort == 'collaborators') {
-      req.session.data.people.sort((a, b) => a.collaborators - b.collaborators);
+
+      req.session.data.people.sort((a, b) => a.collaborators.length - b.collaborators.length);
+
     } else if (req.session.data.sort == 'random') {
+
       req.session.data.people.sort(() => 0.5 - Math.random()) // Shuffle
+
     }
     
   }
   
-  // v.4: Combine with other data
-  if (req.session.data.version > 3) {
+  // v.4: Combine with other data (if not done already)
 
-    // This could be any data - usually not the same dataset data
+  if (req.session.data.version > 3 && !req.session.data.collaborators) {
+
+    // This could be any data - usually not the same data
     // e.g. comments, projects, tags, etc
     // But for the sake of demonstration we'll use our existing dataset again
-
-    let peopleClone = [...req.session.data.people]
     
-    req.session.data.people.forEach(function(p, i) {
+    // For each person in our people data, configure and add further data
+    req.session.data.people.forEach(function(p) {
       
-      let collaborators = [...peopleClone]
-      console.log(`I'm collaborator ${i}`)
-
-      console.log(`Num collaborators: ${collaborators.length}`)
+      // Typically we might load other JSON or use a different dataset here
+      // Be aware though that often we have to create a deep clone here
+      // (due to arrays being stored as references) - which is what the rather
+      // convoluted JSON.parse/stringify combination does
+      let collaborators = JSON.parse(JSON.stringify(req.session.data.people))
       
-      // Remove this person from the collaborators!
+      // Remove the current person from the collaborators because they can't
+      // collaborate with themselves!
       collaborators = collaborators.filter(c => c._uid != p._uid)
-      console.log(`Num collaborators: ${collaborators.length}`)
 
-      // Only use collaborators who were working during the same time period
+      // Only use collaborators who were still working when the current person
+      // started working e.g. no end date or a later end date
       collaborators = collaborators.filter(c => !c.employment.end_date || c.employment.end_date > p.employment.start_date)
-      console.log(`Num collaborators: ${collaborators.length}`)
       
-      // Get up 2-10 random number of collaborators
+      // Choose 2-10 random number of collaborators
       collaborators = collaborators.sort(() => 0.5 - Math.random()) // Shuffle
-      let numCollaborators = Math.floor(Math.random() * 8) + 2
-      collaborators = collaborators.slice(0, numCollaborators)
-      console.log(`Num collaborators: ${collaborators.length}`)
+      let numCollaborators = Math.floor(Math.random() * 8) + 2 // How many
+      collaborators = collaborators.slice(0, numCollaborators) // Remove some
       
       // Add the collaborators to the person data
-      // p.collaborators = collaborators
+      p.collaborators = collaborators
 
     });
+
+    // Add a flag so that we can skip this on subsequent page loads
+    req.session.data.collaborators = true
 
   }
 
